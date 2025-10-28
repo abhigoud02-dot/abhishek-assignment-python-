4069d5df251fc9da452568284e953124d7bd7c92
abhigoud02-dot <abhigoud02@gmail.com>
2025-10-28 14:01:56 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,36 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+>>> #Task 1: Perform Basic Mathematical Operations
+... 
+>>> num1 = 5
+>>> num1
+5
+>>> 
+>>> num2 = 10
+>>> num2
+10
+>>> 
+>>> #Addition
+>>> 
+>>> num1 + num2
+15
+>>> 
+>>> # Subtraction
+>>> 
+>>> num1 - num2
+-5
+>>> 
+>>> #Multiplication
+... 
+>>> num1 * num2
+50
+>>> 
+>>> #Division
+>>> 
+>>> num 1 / num2
+SyntaxError: invalid syntax
+>>> 
+>>> num1 / num2
+0.5
+>>> 
---
+++ Task2.py
@@ -0,0 +1,13 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+>>> name = "John"
+>>> name
+'John'
+>>> 
+>>> last_name = "Doe"
+>>> last_name
+'Doe'
+>>> 
+>>> print("Hello,", name , last_name + "!Welcome to the Python program.")
+Hello, John Doe!Welcome to the Python program.
